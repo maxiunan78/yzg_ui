906ccb1e5ef857a964b7c35f08fa7611e20d0bb5
maxiunan <maxiunan@365yzg.com>
2021-12-22 14:27:29 +0800

    更改

--- testcase/test_register.py
+++ testcase/test_register.py
@@ -8,11 +8,12 @@ import time
 import allure
 import pytest
 
-from yzg_ui.base import yaml_handle
-from yzg_ui.common import custom
-from yzg_ui.common.log import Logger
-from yzg_ui.page.register import Register
-from yzg_ui.common.db_mysql import DB_sql
+from base import yaml_handle
+from common import custom
+from common.log import Logger
+from page.register import Register
+from common.db_mysql import DB_sql
+from time import sleep
 log = Logger()
 
 
@@ -42,21 +43,41 @@ class TestRegister:
         print(sql)
         result = DB_sql().select_db(sql=sql)
         with allure.step(U'检查是否注册'):
-            if (result is not None):
-                # start.get_browser().execute_script("alert('已经注册了！');")
-                log.info("已经注册了！不再继续执行")
-                assert "已经注册了！不再继续执行"
+            assert result == None ,log.info("已经注册了！不再继续执行注册用例")
 
         with allure.step(U'填入必填项，会员名、手机号、验证码'):
             start.input_name(case[u'会员名'])
             start.input_phone(case[u'手机号'])
-            start.input_code(code_url=yaml_handle.param_value('code_url'),pars=yaml_handle.param_value('pars'))
+            dict1 = yaml_handle.param_dict('stationId', 'hqId')
+            dict1.update({'phoneNum': case[u'手机号']})
+            print(dict1)
+            code = start.get_code(code_url=yaml_handle.param_value('code_url'),pars=dict1)
+            assert code is not None, log.info("发送短信失败")
+            start.input_code(code)
+            # assert start.find_element(('xpath',start.Element[u'验证码输入框'])).get_attribute("data-index") == code
 
         with allure.step(u'选择站点'):
             pars = yaml_handle.param_value('pars')
-            sql = "SELECT station_name FROM erp_station.station WHERE STATION_ID={}".format(pars['stationId'])
+            sql = "SELECT station_name FROM erp_station.station WHERE STATION_ID={}".format(yaml_handle.param_value('stationId'))
             result = DB_sql().select_db(sql=sql)
             start.choice_station(result['station_name'])
+
+        with allure.step(u'确认提交'):
+            start.submit()
+            sleep(0.5)
+            sql =  "select * from  crm.member  where PHONE_NUM = '{}' and `STATUS` = 1 and MEMBER_TYPE=1 and station_id={} order by created_time DESC LIMIT 1 " \
+                   .format(case[u'手机号'],yaml_handle.param_value('stationId'))
+            result = DB_sql().select_db(sql=sql)
+            #等待0.0秒后数据库里面没有短信，再次等待1秒钟再进行断言
+            if result is None:
+                sleep(1)
+                result = DB_sql().select_db(sql=sql)
+            assert result is not None ,log.info("注册失败")
+
+
+
+
+
 
 
 if __name__ == "__main__":
